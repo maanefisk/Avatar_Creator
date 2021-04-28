a824fef635c5ad08eaf83b78dbc35220c6b5f7e8
maanefisk <62015771+maanefisk@users.noreply.github.com>
2021-04-28 16:59:03 +0200

    different tests

--- model.js
+++ model.js
@@ -3,4 +3,10 @@ function changingcolor(thecolorpicker) {
     var thebackground = document.getElementById("BG");
     thebackground.style.fill = thecolorpicker.value;
 }
+function category(chosencategory) {
+    console.log(chosencategory.innerText.toLowerCase());
+}
+function thenumber(chosennumber) {
+    console.log(chosennumber.innerText);
+}
 
