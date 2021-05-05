be4b7e2db150780b4756012fd0db9551ff53b61d
maanefisk <62015771+maanefisk@users.noreply.github.com>
2021-05-05 09:02:00 +0200

    now generates menu

--- model.js
+++ model.js
@@ -12,27 +12,33 @@ function thenumber(chosennumber) {
 
 /*List items and their numbers*/
 /*console.log(categories[categorieslist-1]+(categorieslist-1));*/
-
 var categories = ["hair","neck","head","ears","shirt","eyes","pupils","brows","nose","lips","mouth","bangs","background"];
 var categorieslist = 0;
 var i;
 var textofcategories = "";
 var chosencategory = "";
 
-for (i = 0; i < categories.length; i++) {
-    categorieslist++;
-    chosencategory = categories[categorieslist-1];
 
-    textofcategories += "<div id=\""
-        +chosencategory+
-        "\" className=\"menutext\" onClick=\"category(this)\">"
-        +chosencategory +"</div>"+"<br>";
+function generateview() {
+    let column2 = document.getElementById("column2");
+    for (i = 0; i < categories.length; i++) {
+        categorieslist++;
+        chosencategory = categories[categorieslist-1];
+
+        textofcategories += "<div id=\""
+            +chosencategory+
+            "\" class=\"menutext\" onClick=\"category(this)\">"
+            +chosencategory +"</div>"+"<br>";
+    }
+    textofcategories += "<div id=\"save\" class=\"menutext save\">save</div>";
+
+    console.log(textofcategories);
+
+    column2.innerHTML += textofcategories;
+
 }
-textofcategories += "<div id=\"save\" class=\"menutext save\">save</div>";
 
-console.log(textofcategories);
 
-document.getElementById("column2").innerHTML += textofcategories;
-document.getElementById("test").innerHTML += textofcategories;
-/*
-document.getElementById("demo").innerHTML = text;*/
+
+
+
