4c41f1917cb7afb5dd74e954e6455e24d6403b39
maanefisk <62015771+maanefisk@users.noreply.github.com>
2021-05-05 13:57:39 +0200

    now generates numbers

---
+++ script.js
@@ -0,0 +1,109 @@
+const model = {
+    categories: ["hair","neck","head","ears","shirt","eyelids","eyes","iris","pupils","brows","nose","lips","mouth","bangs","background"],
+    counter : 0,
+    activecategory: 'background',
+
+    hair: 12,
+    neck: 2,
+    head: 2,
+    ears: 4,
+    shirt: 3,
+    eyelids: 1,
+    eyes: 10,
+    iris: 1,
+    pupils: 2,
+    brows: 10,
+    nose: 15,
+    lips: 11,
+    mouth: 6,
+    bangs: 7,
+    background: 1,
+};
+
+/*List items and their numbers*/
+/*console.log(categories[counter-1]+(counter-1));*/
+
+var i;
+var textofcategories = "";
+var chosencategory = "";
+var counter = model.counter;
+var categories = model.categories;
+var activecategory = model.activecategory;
+let categoryathand;
+let numberathand;
+let numberlastathand;
+let hair = 12;
+let neck = 2;
+let head = 2;
+let ears = 4;
+let shirt = 3;
+let eyelids = 1;
+let eyes = 10;
+let iris = 1;
+let pupils = 2;
+let brows = 10;
+let nose = 15;
+let lips = 11;
+let mouth = 6;
+let bangs = 7;
+let background = 1;
+
+function changingcolor(thecolorpicker) {
+    var thebackground = document.getElementById("BG");
+    thebackground.style.fill = thecolorpicker.value;
+}
+function category(chosencategory) {
+    activecategory = chosencategory.innerText.toLowerCase();
+    categoryathand = activecategory;
+    generatenumbersview(activecategory);
+}
+function thenumber(chosennumber) {
+    numberlastathand = numberathand;
+    numberathand = chosennumber.innerText;
+    newoelement = categoryathand+numberathand;
+    oldoelement = categoryathand+numberlastathand;
+
+
+    let elementathand = document.getElementById(newoelement);
+    let elementlastathand = document.getElementById(oldoelement);
+
+    elementathand.classList.remove('bydefault');
+    elementathand.classList.add('show');
+    elementlastathand.classList.add('bydefault');
+    elementlastathand.classList.remove('show');
+
+    /*let elementtochange = document.querySelector(classofelement);
+    elementtochange.classList.toggle('bydefault');*/
+}
+
+function generateview() {
+    let column2 = document.getElementById("column2");
+
+    for (i = 0; i < categories.length; i++) {
+        counter++;
+        chosencategory = categories[counter-1];
+
+        textofcategories += "<div id=\""
+            +chosencategory+
+            "\" class=\"menutext\" onClick=\"category(this)\">"
+            +chosencategory +"</div>";
+    }
+    textofcategories += "<div id=\"save\" class=\"menutext save\">save</div>";
+
+    column2.innerHTML += textofcategories;
+}
+
+function generatenumbersview(activeone) {
+    let n;
+    let numbersholder = document.getElementById("numbersholder");
+    var textofnumbers = "";
+    let activenumber = eval(activeone);
+
+    for (n = 1; n < activenumber+1; n++) {
+        textofnumbers += "<div id=\"number"+n+"\" class=\"number\" onclick=\"thenumber(this)\">"+n+"</div>";
+    }
+
+    numbersholder.innerHTML = textofnumbers;
+}
+
+
